4876f8ac29f589e8eb43099218d0fbdbbfb4c87c
Wei Ji <monokuroai@gmail.com>
2019-07-02 11:03:50 -0400

    added post route for new graffiti

--- server/api/location.js
+++ server/api/location.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const {Tag} = require('../db/models')
 const sequelize = require('sequelize')
 const Op = sequelize.Op
+
 router.get('/', async (req, res, next) => {
   const lat = parseFloat(req.query.lat)
   const long = parseFloat(req.query.long)
@@ -22,4 +23,17 @@ router.get('/', async (req, res, next) => {
     next(error)
   }
 })
+
+router.post('/', async (req, res, next) => {
+  try {
+    let lat = req.body.lat
+    let long = req.body.long
+    let arTagUrl = req.body.arTagUrl
+
+    await Tag.create({lat, long, arTagUrl})
+  } catch (error) {
+    next(error)
+  }
+})
+
 module.exports = router
